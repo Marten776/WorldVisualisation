4b251682df0bf7d653d98e76e74d393268ba356b
Marten776 <62341260+Marten776@users.noreply.github.com>
2020-12-16 18:58:07 +0100

    rabbit drinking

--- Assets/Scripts/Rabbit.cs
+++ Assets/Scripts/Rabbit.cs
@@ -5,10 +5,59 @@ using UnityEngine;
 public class Rabbit : MonoBehaviour
 {
     public Rigidbody rb;
+    public float range = 10f;
+
+    public string waterTag = "Water";
+    public float thirstiness = 15f;
+
+    void Start()
+    {
+        InvokeRepeating("waterNeed", 0f, 6f);
+    }
+    void WaterSpoting()
+    { 
+        GameObject[] water = GameObject.FindGameObjectsWithTag(waterTag);
+        foreach (GameObject w in water)
+        {
+            Vector3 waterPosition = w.transform.position;
+            float distanceToWater = Vector3.Distance(transform.position, w.transform.position);
+
+            float drinking = 1f;
+            if (distanceToWater < range)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, waterPosition, 8f * Time.deltaTime);
+                if (distanceToWater < drinking)
+                {
+                    thirstiness = 100f;
+                }
+
+            }
+        }   
+    }
+    void waterNeed()
+    {
+        thirstiness -= 5f;
+        Debug.Log("This rabbit thirstiness: " + thirstiness);
+    }
     void Update()
     {
 
-        rb.AddForce(0, 0, 1200 * Time.deltaTime);
+        if(thirstiness < 50f)
+        {
+            if (GameObject.FindGameObjectWithTag(waterTag) != null)
+                WaterSpoting();
+        }
 
+        if(thirstiness == 0)
+        {
+            Destroy(gameObject);
+            Debug.Log("The rabbit died of thirst");
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
     }
 }
